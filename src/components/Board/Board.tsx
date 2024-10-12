424e1cc9e8c897e592d846601578fa5ee8ec7164
Youssef Elsayed <chessidev@gmail.com>
2024-10-12 23:25:44 +0300

    Pieces

--- src/components/Board/Board.tsx
+++ src/components/Board/Board.tsx
@@ -8,28 +8,32 @@ const Board = () => {
       <div>
         {ranks.map((rank) => {
           return (
-            <div className="grid grid-cols-8">
+            <div key={rank} className="grid grid-cols-8">
               {files.map((file, index) => {
                 return (
                   <div
-                    className={`cursor-pointer w-[10vw] max-w-[75px] aspect-square relative ${
+                    key={file + rank}
+                    className={`square ${
                       (filesNumbers[index] % 2 === 0 && rank % 2 === 0) ||
                       (filesNumbers[index] % 2 !== 0 && rank % 2 !== 0)
                         ? "bg-primary text-secondary"
                         : "bg-secondary text-primary"
                     }`}
                   >
-                    <span className="sr-only">{`${file}${rank}`}</span>
-                    <span
-                      className={`absolute sm:left-1 left-[1px] bottom-[1px] text-xs sm:bottom-1 sm:text-sm`}
-                    >
-                      {rank === 1 ? `${file}` : ""}
-                    </span>
-                    <span
-                      className={`absolute top-[1px] right-[1px] sm:right-1 sm:top-1 text-xs sm:text-sm`}
-                    >
-                      {filesNumbers[index] === 8 ? `${rank}` : ""}
-                    </span>
+                    {rank === 1 ? (
+                      <span className="absolute sm:left-1 left-[1px] bottom-[1px] text-xs sm:bottom-1 sm:text-sm">
+                        {file}
+                      </span>
+                    ) : (
+                      ""
+                    )}
+                    {filesNumbers[index] === 8 ? (
+                      <span className="absolute top-[1px] right-[1px] sm:right-1 sm:top-1 text-xs sm:text-sm">
+                        {rank}
+                      </span>
+                    ) : (
+                      ""
+                    )}
                   </div>
                 );
               })}
